173b89ff482eed26ae3331ef27c10de193b5925e
gaurav pai <gpprojects511@gmail.com>
2025-07-23 21:25:32 +0530

    Example comparison of async await and promise Handling

---
+++ async_await.js
@@ -0,0 +1,34 @@
+/*const p = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve("Promise P resolved ");
+  }, 10000);
+});
+
+const handlePromise = function () {
+  console.log("Welcome to namaste JS");
+  p.then((data) => {
+    console.log(data); // callback
+  });
+  console.log("Bye namaste js");
+};
+handlePromise();
+---output------------
+Welcome to namaste JS
+Bye namaste js
+Promise P resolved  */
+
+const p1 = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve("Promise P1 resolved ");
+    }, 10000);
+  });
+
+async function handlePromise(){
+    console.log("Welcome to namaste JS");
+    const data = await p1;
+    console.log(data);
+    console.log("Bye namaste js");
+
+
+}  
+handlePromise();
